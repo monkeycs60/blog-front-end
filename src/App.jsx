6fecb16ac0ac351a57fdcd9e4c24e5b537e2e52f
monkeycs60 <clement.serizay@gmail.com>
2023-01-28 15:17:10 +0100

    routage basique

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Layout from "./components/Layout/Layout";
 import { Route, Routes, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/error404" element={<NotFoundPage />} />
         <Route path="*" element={<Navigate to={"/error404"} />} />
       </Routes>
     </Layout>
